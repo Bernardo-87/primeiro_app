ed40b28fdf6a1bd7671911677d3b6fbca4762751
Bernardo <bernardo.com5@gmail.com>
2023-05-29 14:10:31 -0300

    tela sobre

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -7,61 +7,62 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("About Page"),),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        backgroundColor: Colors.black.withOpacity(0.1),
-                        title: const Center(
-                          child: Text(
-                            'Primeiro App',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        actions: [
-                          Center(
-                            child: Column(
-                              children: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('---------------------------', style: TextStyle(color: Colors.white12),),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('Desenvolvido por:\n\nBernardo Rezende'),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('---------------------------', style: TextStyle(color: Colors.white12),),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('Version 1.0.0'),
-                                ),
-                              ],
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            opacity: 50,
+            image: AssetImage("assets/images/img.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          backgroundColor: Colors.white12,
+                          title: const Center(
+                            child: Text(
+                              'Primeiro App',
+                              style: TextStyle(color: Colors.white),
                             ),
                           ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                child: const Text("Sobre o App")),
-          ],
+                          actions: [
+                            Center(
+                              child: Column(
+                                children: <Widget>[
+                                  const SizedBox(height: 15),
+                                  const Text('-----------------------------------', style: TextStyle(color: Colors.white24),),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Desenvolvido por:\n\nBernardo Rezende'),
+                                  ),
+                                  const Text('-----------------------------------', style: TextStyle(color: Colors.white24),),
+                                  const SizedBox(height: 15),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Version 1.0.0'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: const Text("Sobre o App")),
+            ],
+          ),
         ),
       ),
     );
